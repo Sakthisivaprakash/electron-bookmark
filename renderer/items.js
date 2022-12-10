c7a380ac886807e80981113547ae5e45ccc06ba7
Sakthisivaprakash <sakthisivaprakash@gmail.com>
2022-12-10 23:49:53 +0530

    Delete Items using post message in render process

--- renderer/items.js
+++ renderer/items.js
@@ -11,6 +11,58 @@ fs.readFile(`${__dirname}/reader.js`, (err, data) => {
 //  Track items in storage
 exports.storage = JSON.parse(localStorage.getItem('readit-items')) || [];
 
+// Listen for "done" message from reader window
+window.addEventListener('message', e => {
+    // Check for correct message
+    if(e.data.action === 'delete-reader-item') {
+        // Delete item at given index
+        this.delete(e.data.itemIndex);
+
+        // Close the reader window
+        console.log(e.source);
+        e.source.close()
+    }
+});
+
+exports.delete = itemIndex => {
+
+    // Remoce item from DOM
+    items.removeChild(items.childNodes[itemIndex]);
+
+    // Remove item from storage
+    this.storage.splice(itemIndex, 1);
+
+    // Persist Storage
+    this.save();
+
+    // Select previous item or new top item
+    if(this.storage.length){
+
+        // Get new selected item idx
+        let = newSelectedItemIndex = (itemIndex === 0) ? 0: itemIndex - 1;
+
+        // Select item at new index
+        document.getElementsByClassName('read-item')[newSelectedItemIndex].classList.add('selected');
+    }
+}
+
+// Get selected item index
+exports.getSelectedItem = () => {
+
+    // get selected node
+    let currentItem  = document.getElementsByClassName('read-item selected')[0];
+
+    // GEt item idx
+    let itemIndex = 0;
+    let child = currentItem;
+    while((child = child.previousElementSibling) != null) itemIndex++;
+
+    // Return selected item and index
+    return {
+        node: currentItem, index: itemIndex
+    }
+}
+
 //  persist storage
 exports.save = () => {
     localStorage.setItem('readit-items', JSON.stringify(this.storage));
@@ -29,15 +81,16 @@ exports.select = e => {
 exports.changeSelection = direction => {
 
     // Get Selected item
-    let currentItem = document.getElementsByClassName('read-item selected')[0];
+    // let currentItem = document.getElementsByClassName('read-item selected')[0];
+    let currentItem = this.getSelectedItem();
 
     // Handle up/down
-    if(direction === 'ArrowUp' && currentItem.previousElementSibling) {
-        currentItem.classList.remove('selected');
-        currentItem.previousElementSibling.classList.add('selected');
-    } else if(direction === 'ArrowDown' && currentItem.nextElementSibling) {
-        currentItem.classList.remove('selected');
-        currentItem.nextElementSibling.classList.add('selected');
+    if(direction === 'ArrowUp' && currentItem.node.previousElementSibling) {
+        currentItem.node.classList.remove('selected');
+        currentItem.node.previousElementSibling.classList.add('selected');
+    } else if(direction === 'ArrowDown' && currentItem.node.nextElementSibling) {
+        currentItem.node.classList.remove('selected');
+        currentItem.node.nextElementSibling.classList.add('selected');
     }
 }
 
@@ -48,10 +101,10 @@ exports.open = () => {
     if(!this.storage.length) return;
 
     // get selected item
-    let selectedItem = document.getElementsByClassName('read-item selected')[0];
+    let selectedItem = this.getSelectedItem();
 
     // get items url
-    let contentURL = selectedItem.dataset.url;
+    let contentURL = selectedItem.node.dataset.url;
 
     // Open item in proxt window
     console.log('Opening item:', contentURL);
@@ -65,8 +118,8 @@ exports.open = () => {
         contextIsolation=1
     `);
 
-    // Inject JS
-    readerWin.eval(readerJS);
+    // Inject JS with specific item index (selecteITem.index)
+    readerWin.eval(readerJS.replace('{{index}}', selectedItem.index));
 }
 
 //  Add new Item
